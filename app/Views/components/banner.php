81cf22638dd2b02a04e8dadcda5b8cc45118b926
CassimClick <cassims44@gmail.com>
2021-08-10 19:37:53 +0300

    initial commit

---
+++ app/Views/components/banner.php
@@ -0,0 +1,74 @@
+<!-- Banner Section -->
+<section class="banner-section">
+    <!-- Social Nav -->
+    <ul class="social-nav">
+        <li class="facebook"><a href="#"><span class="fa fa-facebook-f"></span></a></li>
+        <li class="twitter"><a href="#"><span class="fa fa-twitter"></span></a></li>
+        <li class="linkedin"><a href="#"><span class="fa fa-linkedin"></span></a></li>
+    </ul>
+    <div class="main-slider-carousel owl-carousel owl-theme">
+
+        <div class="slide" style="background-image: url(images/main-slider/image-1.jpg)">
+            <div class="auto-container">
+
+                <!-- Content Column -->
+                <div class="content-column">
+                    <div class="inner-column">
+                        <div class="title">Counsel LAw Firm</div>
+                        <h1>Best LAw Firm <br> Since 1968</h1>
+                        <div class="text">Quis autem velo eum iure reprehenderit rui in ea voluatacera suam nihil
+                            molestiae conseuatur vel illum.</div>
+                        <div class="btns-box">
+                            <a href="contact.html" class="theme-btn btn-style-one"><span class="txt">Free Consultation
+                                    <i class="arrow flaticon-right"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="slide" style="background-image: url(images/main-slider/image-1.jpg)">
+            <div class="auto-container">
+
+                <!-- Content Column -->
+                <div class="content-column">
+                    <div class="inner-column">
+                        <div class="title">Counsel LAw Firm</div>
+                        <h1>Best LAw Firm <br> Since 1968</h1>
+                        <div class="text">Quis autem velo eum iure reprehenderit rui in ea voluatacera suam nihil
+                            molestiae conseuatur vel illum.</div>
+                        <div class="btns-box">
+                            <a href="contact.html" class="theme-btn btn-style-one"><span class="txt">Free Consultation
+                                    <i class="arrow flaticon-right"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="slide" style="background-image: url(images/main-slider/image-1.jpg)">
+            <div class="auto-container">
+
+                <!-- Content Column -->
+                <div class="content-column">
+                    <div class="inner-column">
+                        <div class="title">Counsel LAw Firm</div>
+                        <h1>Best LAw Firm <br> Since 1968</h1>
+                        <div class="text">Quis autem velo eum iure reprehenderit rui in ea voluatacera suam nihil
+                            molestiae conseuatur vel illum.</div>
+                        <div class="btns-box">
+                            <a href="contact.html" class="theme-btn btn-style-one"><span class="txt">Free Consultation
+                                    <i class="arrow flaticon-right"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<!-- End Banner Section -->
---
+++ app/Views/pages/homePage.php
@@ -0,0 +1,4 @@
+<?=$this->extend('layouts/mainLayout');?>
+<?=$this->section('content');?>
+<?=$this->include('components/banner')?>
+<?=$this->endSection();
